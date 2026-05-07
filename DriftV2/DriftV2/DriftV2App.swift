cae77104a62b0f5aac56d3b249fee913031e8080
angel zambrano <angeldzambrano99@gmail.com>
2026-05-07 13:39:17 -0700

    added base project

---
+++ DriftV2/DriftV2/DriftV2App.swift
@@ -0,0 +1,17 @@
+//
+//  DriftV2App.swift
+//  DriftV2
+//
+//  Created by angel zambrano on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DriftV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
